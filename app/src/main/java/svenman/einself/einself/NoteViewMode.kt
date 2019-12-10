3af829e28b09f02ed650e7d9004c804a230f03e2
sveinnd <sveinndpratama@gmail.com>
2019-12-11 01:48:33 +0700

    elf

---
+++ app/src/main/java/svenman/einself/einself/NoteViewMode.kt
@@ -0,0 +1,28 @@
+package svenman.einself.einself
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import svenman.einself.einself.catatan.Note
+import svenman.einself.einself.catatan.NoteRepository
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+    private var repository: NoteRepository =
+        NoteRepository(application)
+    private var allNotes: LiveData<List<Note>> = repository.getAllNotes()
+    fun insert(note: Note) {
+        repository.insert(note)
+    }
+    fun update(note: Note) {
+        repository.update(note)
+    }
+    fun delete(note: Note) {
+        repository.delete(note)
+    }
+    fun deleteAllNotes() {
+        repository.deleteAllNotes()
+    }
+    fun getAllNotes(): LiveData<List<Note>> {
+        return allNotes
+    }
+}
---
+++ app/src/main/java/svenman/einself/einself/catatan/Note.kt
@@ -0,0 +1,15 @@
+package svenman.einself.einself.catatan
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "note_table")
+data class Note(
+    var title: String,
+    var description: String,
+    var priority: Int
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+}
